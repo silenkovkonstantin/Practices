c2b7e47fcc7f9535ec1d9c9551afe385a8434b60
silenkovkonstantin <silenkov@inbox.ru>
2023-04-20 19:14:04 +0500

    Добавьте файлы проекта.

---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Multiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.That(calculator.Multiplication(8, 2) == 16);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.That(calculator.Subtraction(8, 2) == 6);
+        }
+
+        [Test]
+        public void Division__MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.That(calculator.Division(8, 2) == 4);
+        }
+
+        [Test]
+        public void Division_MustThrowException()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(8, 0));
+        }
+
+        [Test]
+        public void Addition__MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+
+            Assert.That(calculator.Addition(8, 2) == 10);
+        }
+
+        
+    }
+}
---
+++ Practices/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
